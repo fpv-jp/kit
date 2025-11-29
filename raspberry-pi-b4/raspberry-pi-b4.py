e11a84da36fe745b2b04d8d2c386069311900b0f
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-11-29 18:31:54 +0900

    xxx

--- raspberry-pi-b4/raspberry-pi-b4.py
+++ raspberry-pi-b4/raspberry-pi-b4.py
@@ -23,10 +23,6 @@ plate_width = 16.4 * 2
 plate_height = 101.0 + 101.8 + M3 * 2
 plate_depth = 2.8
 
-def add_protrusion(target, name, scale, location):
-    base.cube_add(target=target, name=name, scale=scale, location=location)
-
-
 # プレート作成
 plate = base.cube_create(
     name="plate",
@@ -37,41 +33,24 @@ plate = base.cube_create(
 base.cube_clear(
     target=plate,
     name="UpperCornerCut",
-    scale=(plate_width - 12, plate_height - 12, plate_depth),
+    scale=(plate_width - M3 * 8, plate_height - M3 * 8, plate_depth),
     location=(0, 0, 0),
 )
 
-
-# M2.5 リング位置
+# M2.5
 holes = [
     (24.5, 65.0),
     (-24.5, 65.0),
     (24.5, 7),
     (-24.5, 7),
 ]
-
-
 for i, (x, y) in enumerate(holes):
-    add_protrusion(
-        target=plate,
-        name=f"m2_5_mounting_protrusion_{i}",
-        scale=(x * 2, M2_5 * 4, plate_depth),
-        location=(0, y, 0),
-    )
-
-# M2.5 リングの生成
+    base.cube_add(target=plate, name=f"m2_5_mounting_protrusion_{i}", scale=(x * 2, M2_5 * 4, plate_depth), location=(0, y, 0))
 for i, (x, y) in enumerate(holes):
-    base.add_ring(
-        target=plate,
-        name=f"m2_5_ring_{i}",
-        outer_radius=M2_5 * 2,
-        inner_radius=M2_5,
-        location=(x, y, 0),
-        depth=plate_depth,
-    )
+    base.add_ring(target=plate, name=f"m2_5_ring_{i}", outer_radius=M2_5 * 2, inner_radius=M2_5, location=(x, y, 0), depth=plate_depth)
 
 
-# M3 リング位置
+# M3
 holes2 = [
     (16.4, 101.0),
     (-16.4, 101.0),
@@ -82,22 +61,7 @@ holes2 = [
     (18.9, -101.8),
     (-18.9, -101.8),
 ]
-
 for i, (x, y) in enumerate(holes2):
-    add_protrusion(
-        target=plate,
-        name=f"m3_mounting_protrusion_{i}",
-        scale=(x * 2, M3 * 4, plate_depth),
-        location=(0, y, 0),
-    )
-
-# M3 リングの生成
+    base.cube_add(target=plate, name=f"m3_mounting_protrusion_{i}", scale=(x * 2, M3 * 4, plate_depth), location=(0, y, 0))
 for i, (x, y) in enumerate(holes2):
-    base.add_ring(
-        target=plate,
-        name=f"m3_ring_{i}",
-        outer_radius=M3 * 2,
-        inner_radius=M3,
-        location=(x, y, 0),
-        depth=plate_depth,
-    )
+    base.add_ring(target=plate, name=f"m3_ring_{i}", outer_radius=M3 * 2, inner_radius=M3, location=(x, y, 0), depth=plate_depth)
