95475275fe8b0be6408f28e88c11eb01f77af2b9
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-12-24 08:11:34 +0900

    test

--- raspberry-pi-b4/gnss_mount.py
+++ raspberry-pi-b4/gnss_mount.py
@@ -34,64 +34,6 @@ frame = base.cube_create(
     location=(0, 0, 0),
 )
 
-
-########################################
-
-M3 = 1.75
-
-
-PITCH = 18.9
-
-y = 10
-z = -frame_depth / 2
-
-base.cube_add(
-    target=frame,
-    scale=(PITCH * 2, M3 * 4, frame_thickness),
-    location=(0, y, z),
-)
-
-holes = [
-    (-PITCH, y),
-    (PITCH, y),
-]
-for i, (x, y) in enumerate(holes):
-    base.ring_add(
-        target=frame,
-        outer_radius=M3 * 2,
-        inner_radius=M3,
-        location=(x, y, z),
-        depth=frame_thickness,
-    )
-
-########################################
-
-PITCH = 27.0
-
-y = -9
-z = -frame_depth / 2
-
-base.cube_add(
-    target=frame,
-    scale=(PITCH * 2, M3 * 4, frame_thickness),
-    location=(0, y, z),
-)
-
-holes = [
-    (-PITCH, y),
-    (PITCH, y),
-]
-for i, (x, y) in enumerate(holes):
-    base.ring_add(
-        target=frame,
-        outer_radius=M3 * 2,
-        inner_radius=M3,
-        location=(x, y, z),
-        depth=frame_thickness,
-    )
-
-########################################
-
 base.cube_cut(
     target=frame,
     scale=(inner_width, inner_height, frame_depth),
@@ -104,11 +46,72 @@ base.cube_cut(
 )
 base.cube_cut(target=frame, scale=(9.1, 9.0, 9), location=(0, -10.5, 0))
 
+
+M3 = 1.75
+z = -frame_depth / 2
+
 ########################################
+
+PITCH = 18.9
+y = 10
+
+base.cube_add(
+    target=frame,
+    scale=(PITCH * 2, M3 * 4, frame_thickness),
+    location=(0, y, z),
+)
+
+holes = [(-PITCH, y), (PITCH, y)]
+for i, (x, y) in enumerate(holes):
+    base.ring_add(
+        target=frame,
+        outer_radius=M3 * 2,
+        inner_radius=M3,
+        location=(x, y, z),
+        depth=frame_thickness,
+    )
+
+########################################
+
+PITCH = 27.0
+y = -9
+
+base.cube_add(
+    target=frame,
+    scale=(PITCH * 2, M3 * 4, frame_thickness),
+    location=(0, y, z),
+)
+
+holes = [(-PITCH, y), (PITCH, y)]
+for i, (x, y) in enumerate(holes):
+    base.ring_add(
+        target=frame,
+        outer_radius=M3 * 2,
+        inner_radius=M3,
+        location=(x, y, z),
+        depth=frame_thickness,
+    )
+
+#########################################
 
 depth_antenna = 10
 radius_antenn1 = 7.5
 radius_antenn2 = 3.25
+
+PITCH = 17.0
+y = -5
+
+holes = [(-PITCH, y), (PITCH, y)]
+for i, (x, y) in enumerate(holes):
+    base.ring_add(
+        target=frame,
+        outer_radius=6.5,
+        inner_radius=3.25,
+        location=(x, y, z),
+        depth=frame_thickness,
+    )
+
+#########################################
 
 antenna = base.cylinder_create(
     radius=radius_antenn1,
@@ -140,22 +143,13 @@ antenna.rotation_euler = (-math.pi / 6, 0, 0)
 
 base.modifier_apply(obj=antenna, target=frame)
 
-########################################
+base.cube_cut(
+    target=frame,
+    scale=(100, 100, 10),
+    location=(0, 0, -9.25),
+)
 
-base.ring_add(
-    target=frame,
-    outer_radius=6.5,
-    inner_radius=radius_antenn2,
-    depth=frame_thickness,
-    location=(17, -5, -frame_depth / 2),
-)
-base.ring_add(
-    target=frame,
-    outer_radius=6.5,
-    inner_radius=radius_antenn2,
-    depth=frame_thickness,
-    location=(-17, -5, -frame_depth / 2),
-)
+#########################################
 
 erls = 5
 
@@ -176,10 +170,4 @@ base.frame_add(
     location=(-x, y, z - 0.5),
 )
 
-########################################
-
-base.cube_cut(
-    target=frame,
-    scale=(100, 100, 10),
-    location=(0, 0, -9.25),
-)
+#########################################
